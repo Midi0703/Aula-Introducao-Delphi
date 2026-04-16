1153d7a5caf9e3a4c953ca6b7f448581cd5ececd
Midi0703 <midielcastroramires400@gmail.com>
2026-04-16 20:03:12 -0300

    Atv43final

--- UAtividade43.pas
+++ UAtividade43.pas
@@ -17,8 +17,8 @@ type
     edt_horas_mes: TEdit;
     procedure btn_calcularClick(Sender: TObject);
   private
-    var resultado_ir, resultado_inss, resultado_sindicato, resultado_fgts, resultado_liquido, resultado_bruto : Double;
-    var resultado_ir2 : string;
+    var ir, inss, sindicato, fgts, salarioliquido, salariobruto : Double;
+    var ir2 : string;
   public
     { Public declarations }
   end;
@@ -32,45 +32,45 @@ implementation
 
 procedure TfrmAtividade43.btn_calcularClick(Sender: TObject);
 begin
-  resultado_bruto := StrToFloat(edt_valor_hora.Text) * StrToFloat(edt_horas_mes.Text);
+  salariobruto := StrToFloat(edt_valor_hora.Text) * StrToFloat(edt_horas_mes.Text);
 
-  if resultado_bruto <= 1900 then
+  if salariobruto <= 1900 then
   begin
-  resultado_ir := 0 ;
-  resultado_ir2 := 'Isento'
+  ir := 0 ;
+  ir2 := 'Isento'
   end
 
-  else if resultado_bruto <= 3500 then
+  else if salariobruto <= 3500 then
   begin
-  resultado_ir := resultado_bruto - (resultado_bruto *0.95);
-  resultado_ir2 := '5%'
+  ir := salariobruto * 0.05;
+  ir2 := '5%'
   end
 
-  else if resultado_bruto <= 7500 then
+  else if salariobruto <= 7500 then
   begin
-  resultado_ir := resultado_bruto - (resultado_bruto * 0.9);
-  resultado_ir2 := '10%'
+  ir := salariobruto * 0.1;
+  ir2 := '10%'
   end
 
   else
   begin
-  resultado_ir := resultado_bruto - (resultado_bruto *0.8);
-  resultado_ir2 := '20%';
+  ir := salariobruto * 0.20;
+  ir2 := '20%';
   end;
 
-  resultado_inss := resultado_bruto - (resultado_bruto * 0.9);
-  resultado_sindicato := resultado_bruto - (resultado_bruto * 0.97);
-  resultado_liquido := resultado_bruto - resultado_inss - resultado_sindicato - resultado_ir ;
-  resultado_fgts := resultado_bruto - (resultado_bruto * 0.89);
+  inss := salariobruto * 0.1;
+  sindicato := salariobruto * 0.03;
+  fgts := salariobruto * 0.11;
+  salarioliquido := salariobruto - inss - sindicato - ir ;
 
   showmessage('Nome: '+ edt_nome.Text +slinebreak +
-  'Salário Bruto: R$ '+ FormatFloat('0.00',resultado_bruto)+ slinebreak+
-  '(-) IR('+ resultado_ir2 +'): R$ '+FormatFloat('0.00',resultado_ir)+slinebreak+
-  '(-) INSS(10%): R$ '+FormatFloat('0.00',resultado_inss)+slinebreak+
-  '(-) Sindicato(3%): R$ '+FormatFloat('0.00',resultado_sindicato)+slinebreak+
-  'FGTS(11%): R$ '+FormatFloat('0.00',resultado_fgts)+slinebreak+
-  'Total de desconto: R$ '+FormatFloat('0.00',resultado_sindicato+resultado_ir+resultado_inss)+slinebreak+
-  'Salário Líquido: R$ '+ FormatFloat('0.00',resultado_liquido))
+  'Salário Bruto: R$ '+ FormatFloat('0.00',salariobruto)+ slinebreak+
+  '(-) IR('+ ir2 +'): R$ '+FormatFloat('0.00',ir)+slinebreak+
+  '(-) INSS(10%): R$ '+FormatFloat('0.00',inss)+slinebreak+
+  '(-) Sindicato(3%): R$ '+FormatFloat('0.00',sindicato)+slinebreak+
+  'FGTS(11%): R$ '+FormatFloat('0.00',fgts)+slinebreak+
+  'Total de desconto: R$ '+FormatFloat('0.00',sindicato+ir+inss)+slinebreak+
+  'Salário Líquido: R$ '+ FormatFloat('0.00',salarioliquido))
 end;
 
 end.
